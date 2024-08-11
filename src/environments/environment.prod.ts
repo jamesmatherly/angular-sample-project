4f1767025a6f5644b74b05e673d84ce15692e914
jamesmatherly <jamesmatherly@gmail.com>
2024-08-10 21:14:05 -0400

    Updated Action and prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'BACKEND_URL',
+  frontendUrl: 'FRONTEND_URL',
+  cognitoClientId: 'COGNITO_CLIENT_ID',
+  cognitoClientSecret: 'COGNITO_CLIENT_SECRET',
+  cognitoRedirectUri: 'FRONTEND_URL/callback', // Your app's redirect URI
+  cognitoDomain: 'COGNITO_DOMAIN',
+  cognitoUrl: 'COGNITO_URL',
 };
